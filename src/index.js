d1eddde6c7b48a5f54b26ee88aa8da2cc73713a5
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-09-07 11:39:20 -0400

    mock cc submission

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+function renderOneRamen (ramen) {
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+    <img src="${ramen.imgUrl}"> </div> `
+
+    document.querySelector('#ramen-list').appendChild(card)
+}
+
+
+function noodlePics() {
+    fetch('http://localhost:3000/ramens')
+    .then((res) => res.json())
+    .then(ramenData => ramenData.forEach(ramen => renderOneRamen(ramen)));
+}
+
+function render() {
+    noodlePics ()
+    console.log('after')
+}
+
+render();
